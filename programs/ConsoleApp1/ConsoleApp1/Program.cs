8b4616a12acbf12eba33a6e0e7cbe96829e94a17
pavelvasilev8500 <pavelvasilev@live.com>
2020-02-26 14:52:14 +0200

    Create Program.cs

---
+++ programs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student p = new Student();
+            Console.WriteLine(p.Name);
+            Console.WriteLine("\n");
+            Console.WriteLine(p.GetAvgMark());
+            Console.ReadKey();
+        }
+    }
+}
