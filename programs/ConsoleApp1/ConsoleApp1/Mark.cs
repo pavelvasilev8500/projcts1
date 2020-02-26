d242bc3855396ff60bafc94f8e4842912bd82839
pavelvasilev8500 <pavelvasilev@live.com>
2020-02-26 14:53:36 +0200

    Create Mark.cs

---
+++ programs/ConsoleApp1/ConsoleApp1/Mark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Mark
+    {
+        public string Subject
+        {
+            get;
+            set;
+        }
+
+        public string Subject_mark
+        {
+            get;
+            set;
+        }
+    }
+}
