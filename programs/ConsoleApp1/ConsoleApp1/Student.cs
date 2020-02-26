08c5d265e177a210d0e3f1abcb59630c28c8f2ad
pavelvasilev8500 <pavelvasilev@live.com>
2020-02-26 14:54:13 +0200

    Create Student.cs

---
+++ programs/ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        List<double> list = new List<double>();
+        private string name = "Name Surname";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public double GetAvgMark()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                list.Add(i);
+            }
+            double a = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                a = a + list[i];
+            }
+            return a / list.Count;
+        }
+
+        public void ResetAllMarks()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = 0;
+            }
+        }
+    }
+}
