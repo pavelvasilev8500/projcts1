893a736b4d5192e6925e76094a2e90363a82b5d8
pavelvasilev8500 <pavelvasilev@live.com>
2020-03-01 13:03:33 +0200

    Create Mark.cs

---
+++ programs/ConsoleApp1/ConsoleApp1/bin/Debug/netcoreapp3.1/Mark.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Mark
+    {
+
+        public static void Marks(List<double> list)
+        {
+            list.Add(10);
+            list.Add(10);
+            list.Add(10);
+            list.Add(10);
+            list.Add(10);
+        }
+
+
+        List<double> mark = new List<double>();
+        List<string> subject = new List<string>();
+        public string Subject_mark()
+        {
+            Mark.Marks(mark);
+
+            #region Array_of_subjects
+
+            #region Subjects_of_first_Student
+            subject.Add("Q");
+            subject.Add("W");
+            subject.Add("E");
+            subject.Add("R");
+            subject.Add("T");
+            #endregion
+
+            #endregion
+
+            for (int i = 0; i < subject.Count; i++)
+            {
+                Console.WriteLine(subject[i] + " = " + mark[i] + "\n");
+            }
+
+            return "";
+        }
+    }
+}
