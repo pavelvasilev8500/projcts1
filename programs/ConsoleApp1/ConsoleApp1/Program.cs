5cfc64429dc89d7bcf4eb40b86948509bffd67ce
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2020-03-07 19:57:09 +0300

    Update Program.cs

--- programs/ConsoleApp1/ConsoleApp1/Program.cs
+++ programs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace ConsoleApp1
@@ -7,10 +8,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Student p = new Student();
-            Console.WriteLine("Name of student: " + p.Name + "\n" + "\n" + "Mark of student: " +p.GetAvgMark() + "\n");
-            Mark p1 = new Mark();
-            Console.WriteLine(p1.Subject_mark());
+
+            #region student0
+            Student student0 = new Student();
+            student0.studentinfo.AddRange(new string[] { "Student0 ", "Student0 ", "Student0 " });
+            student0.Marks.Add(new Mark ("A", 10));
+            student0.Marks.Add(new Mark ("B", 9));
+            student0.Marks.Add(new Mark ("C", 8));
+            student0.Marks.Add(new Mark ("D", 7));
+            student0.StudentInfo();
+            Console.WriteLine("Average mark " + student0.GetAvgMark() + "\n");
+            #endregion
+
+            #region student1
+            Student student1 = new Student();
+            student1.studentinfo.AddRange(new string[] { "Student1 ", "Student1 ", "Student1 " });
+            student1.Marks.Add(new Mark("A1", 6));
+            student1.Marks.Add(new Mark("B1", 5));
+            student1.Marks.Add(new Mark("C1", 4));
+            student1.Marks.Add(new Mark("D1", 3));
+            student1.StudentInfo();
+            Console.WriteLine("Average mark " + student1.GetAvgMark()+ "\n");
+            #endregion
+
             Console.WriteLine("\n");
             Console.ReadKey();
         }
